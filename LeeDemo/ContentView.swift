2c68e4879563e8a568b43c85361a25372e3af953
LeeeChenn <419094469@qq.com>
2024-05-14 10:56:16 +0800

    Initial Commit

---
+++ LeeDemo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LeeDemo
+//
+//  Created by 陈景利 on 2024/5/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world hi!")
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LeeDemo/LeeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeeDemoApp.swift
+//  LeeDemo
+//
+//  Created by 陈景利 on 2024/5/14.
+//
+
+import SwiftUI
+
+@main
+struct LeeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
